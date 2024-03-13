87cc2aa0a00fd7454638fe4d8f3fcbc4e2f2fb51
Zuzia <zuzia.jarl@gmail.com>
2024-03-13 14:25:27 +0100

    fixed

--- lib/phoenix_hello/validator.ex
+++ lib/phoenix_hello/validator.ex
@@ -2,11 +2,10 @@ defmodule PhoenixHello.Validator do
   @doc """
     Function should return:
       "yes" if a > b
-      "no" if a < b
-
+      "no" if a <= b
   """
   def is_bigger?(a, b) do
-    if a < b do
+    if a > b do
       "yes"
     else
       "no"
